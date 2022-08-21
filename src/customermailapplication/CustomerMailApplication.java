a537d2d9e33fd8ee92a4baa20345883af3e25bff
63050102 <63050102@kmitl.ac.th>
2022-08-21 11:40:37 +0700

    EX1

--- src/customermailapplication/CustomerMailApplication.java
+++ src/customermailapplication/CustomerMailApplication.java
@@ -23,8 +23,14 @@ public class CustomerMailApplication {
             case "Regular":
                 customer = new RegularCustomer();
                 break;
-            //complete MountainCustomer
-            //complete DelinquentCustomer 
+
+            case "Mountain":
+                customer = new MountainCustomer();
+                break;
+
+            case "Delinquent":
+                customer = new DelinquentCustomer();
+                break;
         }
     }
     public String generateMail() {
@@ -34,7 +40,7 @@ public class CustomerMailApplication {
     public static void main(String[] args) {
         CustomerMailApplication app = new CustomerMailApplication();
         Scanner inp = new Scanner(System.in);
-        System.out.print("Please choose customer type 1. Regular, 2. Mountain, 3. Delinquent ");
+        System.out.print("Please choose customer type 1. Regular, 2. Mountain, 3. Delinquent \n");
         int type = inp.nextInt();
         switch(type) {
             case 1:
